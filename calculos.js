7cae8969b3c32026fe5ead6f7c55185ff86d009a
Algargi2003 <93030536+Algargi2003@users.noreply.github.com>
2022-09-22 20:33:27 +0200

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,30 @@
+function calcularProteinas(){
+    let numero1 = document.getElementById("numero1").value;
+    // let numero2 = document.getElementById("numero2").value;
+    // let numero3 = document.getElementById("numero3").value;
+    let numero4 = document.getElementById("numero4").value;
+    let sol;
+    
+        sol = ((numero1*numero4)/100);
+        document.getElementById("solucion").innerHTML = sol;
+    }
+
+
+
+
+
+
+
+
+
+
+    // let palabra = prompt("Mete la palabra");
+    // var palabraSeparada = palabra.split(" ");
+
+    // document.getElementById("palabraEnseñar").innerHTML = palabra;
+    
+    // var palabraFinal = palabraSeparada[0].split("");
+    // for(let i=1;i<palabraFinal.length;i++){
+    //     document.getElementById("mostrarMiniTitulo").innerHTML = "Letra"+(i+1);
+    //     document.getElementById("mostrarDeletreo").innerHTML = palabraFinal[i];
+    // }
